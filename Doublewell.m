e3f0577986d4d309242d5bef090133df37a749b5
liyangnuaa <61436455+liyangnuaa@users.noreply.github.com>
2020-04-05 16:03:58 -0500

    Update Doublewell.m

--- Doublewell.m
+++ Doublewell.m
@@ -5,59 +5,45 @@ xmin=-1;
 xmax=1;
 zmin=-5;
 zmax=5;
-Nz=1e8;
+Nz=5e6;
 z0=linspace(zmin,zmax,Nz);
 
 % Cinterval=1;             %%% 计算漂移系数的积分区间
 xlim=100;                %%% 跳跃振幅截断
-N=1;                   %%% 一个初始点给出的样本数
+% N=1;                   %%% 一个初始点给出的样本数
 h=0.001;
-alpha=0.5;
+alpha=1.5;
 sigma=2;
-xf0=zeros(1,Nz*N);
-xf=zeros(1,Nz*N);
-position=(0:Nz-1)*N;
 
-M=h^(1/alpha)*stblrnd(alpha,0,1,0,1,Nz+1e4);
-I=abs(M)<xlim;
-M=M(I);
-M=M(1:Nz);
-xf(position+1)=(4*z0-z0.^3)*h+sigma*M;
-xf0(position+1)=z0;
+M=h^(1/alpha)*stblrnd(alpha,0,1,0,1,Nz);
+xf=(4*z0-z0.^3)*h+sigma*M;
+xf0=z0;
 
-xpts=linspace(-xlim,xlim,1e3);
-[pdf,x00]=ksdensity(xf,xpts);
-pdf1=pdf/h;
-x2=x00;
-pdf2=pdf1;
-[~,I]=min(abs(x00));
-x2(I)=[];
-pdf2(I)=[];
-[~,I]=min(abs(x00));
-x2(I)=[];
-pdf2(I)=[];
+a=0.2;
+m=5;
+N=2;
+nk=zeros(1,N+1);
+for k=0:N
+    I=(abs(xf)>=m^k*a)&(abs(xf)<m^(k+1)*a);
+    nk(k+1)=length(xf(I));
+end
+nratio=nk(1)./nk(2:end);
+pos=1:N;
+alpha1=log(nratio)./(pos*log(m));
+alpha0=sum(alpha1)/N;
 
-syms t
-% f=fittype('k1/(t+r1)^r2+k2','independent','t','coefficients',{'r1','r2','k1','k2'});
-% cfun=fit(x0,y0,f,'StartPoint',[3,4,-1e5,38]); %显示拟合函数，数据必须为列向量形式
-f=fittype('calpha/abs(t).^(1+alpha0)','independent','t','coefficients',{'calpha','alpha0'});
-cfun=fit(x2',pdf2',f,'StartPoint',[0.3,1]); %显示拟合函数，数据必须为列向量形式
-delta=0.05;
-xi1=-xlim:0.01:-delta;
-xi2=delta:0.01:xlim;
-xi=[xi1 xi2];
-% xi=-xlim:0.01:xlim+0.001;
-yi=cfun(xi);
-figure;
-plot(x00,pdf1,'r*',xi,yi,'b-');
+cnalpha=alpha0*gamma((1+alpha0)/2)/(2^(1-alpha0)*sqrt(pi)*gamma(1-alpha0/2));
+pos=0:N;
+sigmak=a*m.^pos.*(nk*alpha0/(h*Nz*2*cnalpha*(1-m^(-alpha0)))).^(1/alpha0);
+sigma0=sum(sigmak)/(N+1);
 
-xc=0.1:0.01:1.9;
-cnalpha=xc.*gamma((1+xc)/2)./(2.^(1-xc)*sqrt(pi).*gamma(1-xc/2));
-figure;
-plot(xc,cnalpha);
-
+% xc=0.1:0.01:1.9;
+% cnalpha=xc.*gamma((1+xc)/2)./(2.^(1-xc)*sqrt(pi).*gamma(1-xc/2));
+% figure;
+% plot(xc,cnalpha);
+% 
 Ncoef=6;
-I=(xf0<zmax)&(xf0>zmin);
+I=(abs(xf)<xlim);
 zinitial=xf0(I);
 x=xf(I);
 n=length(zinitial);
